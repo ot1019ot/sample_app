e302866a191910f6149a6730bbc682aa92c8ffad
[Finish]3-2'git config --global user.name [Finish]3-2ls desktopstty sane <ot1019ot@gmail.com>
2019-06-11 07:53:36 +0000

    [first]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '774f8f5a43e7d55a98e54349d18db6d38d94f8b83eadb07341be2d04f5c8dfb0cfa828ebfbe8cbd4261a498f8a7ad32dbfc1da0aed3e968d14ee8f7ada315fb3'
+
+
+
